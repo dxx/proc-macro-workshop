7f0f9d517e85f0e1ee4d69958477cace6b0a907f
dxx <dx1898@foxmail.com>
2022-05-20 08:03:53 +0800

    Finish create builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,8 +4,12 @@ use proc_macro::TokenStream;
 #[path = "./01.rs"]
 mod _01;
 
+#[allow(dead_code)]
+#[path = "./02.rs"]
+mod _02;
+
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    _01::token_stream(input)
+    // _01::token_stream(input)
+    _02::token_stream(input)
 }
-
