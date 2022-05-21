ec7b5ec78cfd8efdbe9002e68f899fbcd409dc80
dxx <dx1898@foxmail.com>
2022-05-21 08:04:51 +0800

    Finish call setters

---
+++ builder/src/03.rs
@@ -0,0 +1,151 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{DeriveInput, Error, Ident, spanned::Spanned, parse_macro_input};
+
+pub fn token_stream(input: TokenStream) -> TokenStream {
+    let derive_input = parse_macro_input!(input as DeriveInput);
+
+    let builder_stream = generate_builder(&derive_input);
+    let impl_stream = generate_impl(&derive_input);
+    let stream = quote!{
+        #builder_stream
+        #impl_stream
+    };
+    stream.into()
+}
+
+/// Generate builder struct.
+/// pub struct CommandBuilder { ... }.
+fn generate_builder(input: &DeriveInput) -> proc_macro2::TokenStream {
+    let vis = &input.vis;
+    let builder_name = format!("{}Builder", input.ident.to_string());
+    let builder_ident = Ident::new(&builder_name, input.span());
+
+    let optional_field_stream = parse_optional_fields(input, false);
+    if let Err(err) = optional_field_stream {
+        return err.into_compile_error();
+    }
+    let optional_field_stream = optional_field_stream.unwrap();
+
+    // There will be no error here if the above runs successfully.
+    let setter_stream = generate_setters(input).unwrap();
+
+    quote! {
+        #vis struct #builder_ident {
+            #(#optional_field_stream),*
+        }
+        impl #builder_ident {
+            #(#setter_stream)*
+        }
+    }
+}
+
+/// Generate impl of input struct.
+/// impl Command { ... }.
+fn generate_impl(input: &DeriveInput) -> proc_macro2::TokenStream {
+    let ident = &input.ident;
+    let builder_name = format!("{}Builder", input.ident.to_string());
+    let builder_ident = Ident::new(&builder_name, input.span());
+    let builder_method_stream = generate_builder_method(input, &builder_ident);
+    quote! {
+        impl #ident {
+            #builder_method_stream
+        }
+    }
+}
+
+///  pub fn builder() -> CommandBuilder {
+///      CommandBuilder {
+///          executable: None,
+///          args: None,
+///          env: None,
+///          current_dir: None,
+///      }
+///  }
+fn generate_builder_method(input: &DeriveInput, builder_ident: &Ident) -> proc_macro2::TokenStream {
+    let optional_field_stream = parse_optional_fields(input, true);
+    if let Err(err) = optional_field_stream {
+        return err.into_compile_error();
+    }
+    let optional_field_stream = optional_field_stream.unwrap();
+    quote! {
+        pub fn builder() -> #builder_ident {
+            #builder_ident{
+                #(#optional_field_stream),*
+            }
+        }
+    }
+}
+
+/// Generate setters.
+/// 
+/// fn executable(&mut self, executable: String) -> &mut Self {
+///     self.executable = Some(executable);
+///     self
+/// }
+/// fn args(&mut self, args: Vec<String>) -> &mut Self {
+///     self.args = Some(args);
+///     self
+/// }
+/// ...
+fn generate_setters(input: &DeriveInput) -> syn::Result<Vec<proc_macro2::TokenStream>> {
+    parse_fields(input, |field| {
+        let vis = &field.vis;
+        let ident = &field.ident;
+        let ty = &field.ty;
+        quote! {
+            #vis fn #ident (&mut self, #ident: #ty) -> &mut Self {
+                self.#ident = Some(#ident);
+                self
+            }
+        }
+    })
+}
+
+/// Parse field list.
+/// 
+/// if init is true:
+/// executable: None,
+//  args: None,
+//  env: None,
+//  current_dir: None,
+/// 
+/// if init is false:
+/// executable: Option<String>
+//  args: Option<Vec<String>>
+//  env: Option<Vec<String>>
+//  current_dir: Option<String>
+fn parse_optional_fields(
+    input: &DeriveInput,
+    init: bool
+) -> syn::Result<Vec<proc_macro2::TokenStream>> {
+    parse_fields(input, |field| {
+        let vis = &field.vis;
+        let ident = &field.ident;
+        let ty = &field.ty;
+        let right = if init { quote!(None) } else { quote!(Option<#ty>)};
+        quote! {
+            #vis #ident: #right
+        }
+    })
+}
+
+/// Parse field of struct. Call f function in iteration.
+fn parse_fields(
+    input: &DeriveInput,
+    f: impl Fn(&syn::Field) -> proc_macro2::TokenStream)
+-> syn::Result<Vec<proc_macro2::TokenStream>> {
+    if let syn::Data::Struct(syn::DataStruct{fields, ..}) = &input.data {
+        match fields {
+            syn::Fields::Named(syn::FieldsNamed {named, ..}) => {
+                return Ok(named.iter().map(|field| {
+                    f(field)
+                }).collect());
+            },
+            _ => {
+                return Err(Error::new_spanned(&fields, "unexpected fields"))
+            }
+        }
+    };
+    Err(Error::new_spanned(&input, "unexpected derive input"))
+}
--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,8 +8,13 @@ mod _01;
 #[path = "./02.rs"]
 mod _02;
 
+#[allow(dead_code)]
+#[path = "./03.rs"]
+mod _03;
+
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     // _01::token_stream(input)
-    _02::token_stream(input)
+    // _02::token_stream(input)
+    _03::token_stream(input)
 }
