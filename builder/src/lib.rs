eeb8b16740dc0195f269f04c4ebeaec54f51f339
dxx <dx1898@foxmail.com>
2022-05-23 08:06:53 +0800

    Finish call build

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -12,9 +12,14 @@ mod _02;
 #[path = "./03.rs"]
 mod _03;
 
+#[allow(dead_code)]
+#[path = "./04.rs"]
+mod _04;
+
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     // _01::token_stream(input)
     // _02::token_stream(input)
-    _03::token_stream(input)
+    // _03::token_stream(input)
+    _04::token_stream(input)
 }
