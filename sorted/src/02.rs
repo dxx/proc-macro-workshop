63d5cdd986e531d710d0109d275c40ed9545a308
dxx <dx1898@foxmail.com>
2022-06-12 19:52:20 +0800

    Finish not enum

--- sorted/src/02.rs
+++ sorted/src/02.rs
@@ -4,13 +4,15 @@ use syn::{parse_macro_input, Item};
 
 pub fn token_stream(_args: TokenStream, input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as Item);
-    if let Err(err) = valid_item(&item) {
-        return err.to_compile_error().into();
+    if let Err(err) = parse_item(&item) {
+        let mut err_token_stream = err.to_compile_error();
+        err_token_stream.extend(item.into_token_stream());
+        return err_token_stream.into();
     }
     item.to_token_stream().into()
 }
 
-fn valid_item(item: &Item) -> syn::Result<()> {
+fn parse_item(item: &Item) -> syn::Result<()> {
     if let syn::Item::Enum(_) = item {
         return Ok(());
     }
