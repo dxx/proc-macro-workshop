8fb243d5386a6581a5802a34ff965eff77b65bf2
dxx <dx1898@foxmail.com>
2022-06-13 09:24:38 +0800

    Finish out of order

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -4,11 +4,16 @@ use proc_macro::TokenStream;
 #[path ="./01.rs"]
 mod _01;
 
+#[allow(dead_code)]
 #[path ="./02.rs"]
 mod _02;
+
+#[path ="./03.rs"]
+mod _03;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     // _01::token_stream(args, input)
-    _02::token_stream(args, input)
+    // _02::token_stream(args, input)
+    _03::token_stream(args, input)
 }
