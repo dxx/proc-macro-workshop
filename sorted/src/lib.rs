733841d8176dc7af5a3126f1874c01283234a463
dxx <dx1898@foxmail.com>
2022-06-14 20:45:25 +0800

    Finish match expr

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -12,13 +12,24 @@ mod _02;
 #[path ="./03.rs"]
 mod _03;
 
+#[allow(dead_code)]
 #[path ="./04.rs"]
 mod _04;
+
+#[path ="./05.rs"]
+mod _05;
+
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     // _01::token_stream(args, input)
     // _02::token_stream(args, input)
     // _03::token_stream(args, input)
-    _04::token_stream(args, input)
+    // _04::token_stream(args, input)
+    _05::token_stream(args, input)
+}
+
+#[proc_macro_attribute]
+pub fn check(args: TokenStream, input: TokenStream) -> TokenStream {
+    _05::check_token_stream(args, input)
 }
